8174da7dec117d9d3f56cfd4bb5d8cb6b919cd4b
SlimRoms-togari <mustermaxmueller@gmx.de>
2014-03-31 14:44:35 +0200

    Update aokp.mk

--- aokp.mk
+++ aokp.mk
@@ -1,12 +1,23 @@
-# Inherit common product files.
-$(call inherit-product, vendor/aokp/configs/common.mk)
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
 
-# Inherit GSM common stuff
-$(call inherit-product, vendor/aokp/configs/gsm.mk)
 
-# Inherit AOSP device configuration for togari
 $(call inherit-product, device/sony/togari/full_togari.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6833 BUILD_FINGERPRINT=Sony/C6833_1274-8613/C6833:4.2.2/14.1.B.1.526/8bl_jw:user/release-keys PRIVATE_BUILD_DESC="C6833-user 4.2.2 14.1.B.1.526 8bl_jw test-keys"
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/slim/config/gsm.mk)
+
+# Inherit CM common Phone stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+
+
+PRODUCT_NAME := slim_togari
+PRODUCT_DEVICE := togari
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6833 BUILD_FINGERPRINT=Sony/C6833/C6833:4.3/14.2.A.0.290/eng.hudsonslave:user/release-keys PRIVATE_BUILD_DESC="C6833-user 4.3 RHINE-1.1-131125-1201 eng.hudsonslave test-keys"
 
 TARGET_OTA_ASSERT_SKIP=true
